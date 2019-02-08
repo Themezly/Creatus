030cceaff3e85ece8c5fa5087ceb9b92075c7a54
Dragan <nocniagenti@gmail.com>
2019-02-08 02:39:35 +0100

    added notice for theme plugins

--- inc/includes/utilities/customizer/customizer-defaults.php
+++ inc/includes/utilities/customizer/customizer-defaults.php
@@ -9,7 +9,38 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // No direct access
 }
+/**
+ * https://gist.github.com/NateWr/5fd00beb412f78776e9d
+*/
+class Thz_Customizer_Notice_Control extends WP_Customize_Control {
+	/**
+	* Control type
+	*/
+	public $type = 'thz_customizer_notice';
+	/**
+	* HTML content to display
+	*/
+	public $content = '';
+	public function render_content() {
+	?>
+    <div class="notice notice-success">
+		<?php if ( !empty( $this->label ) ) :	?>
+        <div class="customize-control-title">
+            <?php echo esc_html( $this->label ); ?>
+        </div>
+        <?php endif; ?>
+        <p>
+            <?php echo $this->content; ?>
+        </p>
+    </div>     
+		<?php 
+	}
+	
+}
 
+/**
+ * Default customizer options if framework is not active
+ */
 function thz_action_default_customizer_options( $wp_customize ){
 	
 	$wp_customize->add_panel( 'creatus_default', array(
@@ -19,18 +50,38 @@ function thz_action_default_customizer_options( $wp_customize ){
 		'description' => __('Customize the login of your website.', 'creatus'),
 	));	
 	
-	
 	// hero section
 	$wp_customize->add_section('creatus_default_hero', array(
 		'priority' => 5,
 		'title' => __('Hero Section', 'creatus'),
 		'panel' => 'creatus_default',
 	));
+	
+	// advise that recommended plugins are not installed
+	$wp_customize->add_control(
+		new Thz_Customizer_Notice_Control(
+			$wp_customize,
+			'hero_notice',
+			array(
+				'label'     => __( 'Install Recomended Plugins', 'creatus' ),
+				'section'   => 'creatus_default_hero',
+				'settings'   => array(),
+				'content'   => sprintf(
+					// Translators: 1 and 2 wrap a link to plugins installation utility.
+					__( 'This is a default customizer setup. For advanced theme options Install and activate %1$srecommended plugins%2$s.', 'creatus' ),
+					'<a href="' . esc_url( admin_url( 'themes.php?page=creatus-install-plugins' ) ) . '">',
+					'</a>'
+				),
+			)
+		)
+	);
+	
 	// before title
 	$wp_customize->add_setting('creatus_before_hero_title', array(
 		'default'        => '',
         'capability'     => 'edit_theme_options',
         'type'           => 'option',
+		'sanitize_callback' => 'esc_attr'
 	));
 	$wp_customize->add_control('creatus_before_hero_title', array(
 		'label'   => __('Before Title', 'creatus'),
@@ -44,6 +95,7 @@ function thz_action_default_customizer_options( $wp_customize ){
 		'default'        => '',
         'capability'     => 'edit_theme_options',
         'type'           => 'option',
+		'sanitize_callback' => 'esc_attr'
 	));
 	$wp_customize->add_control('creatus_hero_title', array(
 		'label'   => __('Hero Title', 'creatus'),
@@ -57,6 +109,7 @@ function thz_action_default_customizer_options( $wp_customize ){
 		'default'        => '',
         'capability'     => 'edit_theme_options',
         'type'           => 'option',
+		'sanitize_callback' => 'esc_attr'
 	));
 	$wp_customize->add_control('creatus_hero_subtitle', array(
 		'label'   => __('Sub Title', 'creatus'),
@@ -70,6 +123,7 @@ function thz_action_default_customizer_options( $wp_customize ){
 		'default'        => '',
         'capability'     => 'edit_theme_options',
         'type'           => 'option',
+		'sanitize_callback' => 'esc_attr'
 	));
 	$wp_customize->add_control('creatus_hero_button_text', array(
 		'label'   => __('Button text', 'creatus'),
@@ -83,6 +137,7 @@ function thz_action_default_customizer_options( $wp_customize ){
 		'default'        => '',
         'capability'     => 'edit_theme_options',
         'type'           => 'option',
+		'sanitize_callback' => 'esc_attr'
 	));
 	$wp_customize->add_control('creatus_hero_button_link', array(
 		'label'   => __('Button link', 'creatus'),
@@ -97,12 +152,32 @@ function thz_action_default_customizer_options( $wp_customize ){
 		'title' => __('Branding', 'creatus'),
 		'panel' => 'creatus_default',
 	));	
-	
-	// bradning text
+
+	// advise that recommended plugins are not installed
+	$wp_customize->add_control(
+		new Thz_Customizer_Notice_Control(
+			$wp_customize,
+			'branding_notice',
+			array(
+				'label'     => __( 'Install Recomended Plugins', 'creatus' ),
+				'section'   => 'creatus_default_branding',
+				'settings'   => array(),
+				'content'   => sprintf(
+					// Translators: 1 and 2 wrap a link to plugins installation utility.
+					__( 'This is a default customizer setup. For advanced theme options Install and activate %1$srecommended plugins%2$s.', 'creatus' ),
+					'<a href="' . esc_url( admin_url( 'themes.php?page=creatus-install-plugins' ) ) . '">',
+					'</a>'
+				),
+			)
+		)
+	);
+		
+	// branding text
 	$wp_customize->add_setting('creatus_branding_text', array(
 		'default'        => '',
         'capability'     => 'edit_theme_options',
         'type'           => 'option',
+		'sanitize_callback' => 'esc_attr'
 	));
 	$wp_customize->add_control('creatus_branding_text', array(
 		'label'   => __('Branding text', 'creatus'),
@@ -116,6 +191,7 @@ function thz_action_default_customizer_options( $wp_customize ){
 		'default'        => '',
         'capability'     => 'edit_theme_options',
         'type'           => 'option',
+		'sanitize_callback' => 'esc_attr'
 	));
 	$wp_customize->add_control('creatus_branding_link', array(
 		'label'   => __('Branding link', 'creatus'),
@@ -125,6 +201,5 @@ function thz_action_default_customizer_options( $wp_customize ){
 	));
 	
 }
-
 
 add_action('customize_register', 'thz_action_default_customizer_options');
